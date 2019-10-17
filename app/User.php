1c1ef8bca29c4f90e0a65d703e0172bddc322297
Aleksandar Stojanov <aleksandarstojanov@megantic.net.au>
2019-10-17 14:16:22 +0200

    Generate questio0n model via artisan model generator

--- app/User.php
+++ app/User.php
@@ -36,4 +36,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        // Function to convert the string into slug format
+        $this->attributes['slug'] = str_slug($value);
+    }
 }
