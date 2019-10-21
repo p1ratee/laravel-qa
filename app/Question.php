99a5ff964a2737a5f7ffa2c46f6396048d7d5c01
Aleksandar Stojanov <aleksandarstojanov@megantic.net.au>
2019-10-21 16:04:31 +0200

    Refactored some code to DRY principles

--- app/Question.php
+++ app/Question.php
@@ -10,6 +10,8 @@ use App\Answer;
 
 class Question extends Model
 {
+    use VotableTrait;
+
     protected $fillable = ['title', 'body'];
     // questions belongs to user
     public function user()
@@ -80,20 +82,5 @@ class Question extends Model
     public function getFavoritesCountAttribute()
     {
         return $this->favorites->count();
-    }
-
-    public function votes()
-    {
-        return $this->morphToMany(User::class, 'votable');
-    }
-
-    public function upVotes()
-    {
-        return $this->votes()->wherePivot('vote', 1);
-    }
-
-    public function downVotes()
-    {
-        return $this->votes()->wherePivot('vote', -1);
     }
 }
