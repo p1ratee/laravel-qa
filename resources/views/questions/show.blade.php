09a68e89837d28af5c4fe1ead27509f2f3572622
Aleksandar Stojanov <aleksandarstojanov@megantic.net.au>
2019-10-22 08:13:02 +0200

    Dry code principles

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -18,40 +18,9 @@
                     </div>
                     <hr>
                     <div class="media">
-                        <div class="d-flex flex-column vote-controls">
-                            <a title="This question is useful" class="vote-up {{ Auth::guest() ? 'off' : '' }}"
-                                onclick="event.preventDefault(); document.getElementById('up-vote-question-{{ $question->id }}').submit()">
-                                <i class="fas fa-caret-up fa-3x"></i>
-                            </a>
-                            <form method="POST" action="/questions/{{ $question->id }}/vote"
-                                id="up-vote-question-{{ $question->id }}">
-                                @csrf
-                                <input type="hidden" name="vote" value="1">
-                            </form>
-                            <span class="votes-count">{{ $question->votes_count }}</span>
-                            <a title="I dont like this question" class="vote-down {{ Auth::guest() ? 'off' : '' }}"
-                                onclick="event.preventDefault(); document.getElementById('down-vote-question-{{ $question->id }}').submit()">
-                                <i class="fas fa-caret-down fa-2x"></i>
-                            </a>
-                            <form method="POST" action="/questions/{{ $question->id }}/vote"
-                                id="down-vote-question-{{ $question->id }}">
-                                @csrf
-                                <input type="hidden" name="vote" value="-1">
-                            </form>
-                            <a title="Click to mark as favorite (Click again to undo)"
-                                class="favorite mt-2 {{ Auth::guest() ? 'off' : ($question->is_favorited) ? 'favorited' : '' }}"
-                                onclick="event.preventDefault(); document.getElementById('favorite-question-{{ $question->id }}').submit()">
-                                <i class="fas fa-star fa-2x"></i>
-                                <span class="favorites-count">{{ $question->favorites_count }}</span>
-                            </a>
-                            <form method="POST" action="/questions/{{ $question->id }}/favorites"
-                                id="favorite-question-{{ $question->id }}">
-                                @if ($question->is_favorited)
-                                @method("DELETE")
-                                @endif
-                                @csrf
-                            </form>
-                        </div>
+                        @include('shared._vote',[
+                        'model' => $question
+                        ])
                         <div class="media-body">
                             {!! $question->body !!}
                             <div class="row">
@@ -75,6 +44,6 @@
             ])
             @include('answers._create')
         </div>
-      
+
     </div>
     @endsection
---
+++ resources/views/shared/_vote.blade.php
@@ -0,0 +1,46 @@
+@if ($model instanceof App\Question)
+@php
+$name = 'question';
+$firstUriSegment = 'questions';
+@endphp
+@elseif ($model instanceof App\Answer)
+@php
+$name = 'answer';
+$firstUriSegment = 'answers';
+@endphp
+@endif
+
+@php
+$formId = $name ."-".$model->id;
+$formAction = $firstUriSegment ."/". $model->id ."/vote";
+@endphp
+
+<div class="d-flex flex-column vote-controls">
+    <a title="This {{ $name }} is useful" class="vote-up {{ Auth::guest() ? 'off' : '' }}"
+        onclick="event.preventDefault(); document.getElementById('up-vote-{{ $formId }}').submit();">
+        <i class="fas fa-caret-up fa-3x"></i>
+    </a>
+    <form method="POST" action="{{ $formAction }}" id="up-vote-{{ $formId }}">
+        @csrf
+        <input type="hidden" name="vote" value="1">
+    </form>
+    <span class="votes-count">{{ $model->votes_count }}</span>
+    <a title="I dont like this {{ $name }}" class="vote-down {{ Auth::guest() ? 'off' : '' }}"
+        onclick="event.preventDefault(); document.getElementById('down-vote-{{ $formId }}').submit();">
+        <i class="fas fa-caret-down fa-2x"></i>
+    </a>
+    <form method="POST" action="{{ $formAction }}" id="down-vote-{{ $formId }}">
+        @csrf
+        <input type="hidden" name="vote" value="-1">
+    </form>
+
+    @if ($model instanceOf App\Question)
+    @include('shared._favorite',[
+    'model'=>$model
+    ])
+    @elseif($mode instanceof App\Answer)
+    @include('shared._accept',[
+    'model' => $model
+    ])
+    @endif
+</div>
