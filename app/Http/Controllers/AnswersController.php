8fae0e59614c0810f82aafc0d5a9504b927acb45
Aleksandar Stojanov <aleksandarstojanov@megantic.net.au>
2019-10-22 13:11:17 +0200

    Create vue answer component and functionality

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class AnswersController extends Controller
 {
     /**
-         * Store a newly created resource in storage.
+     * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
@@ -20,7 +20,7 @@ class AnswersController extends Controller
             'body' => 'required'
         ]) + ['user_id' => \Auth::id()]);
 
-        return back()->with('success',"Your answer has been submitted successfully");
+        return back()->with('success', "Your answer has been submitted successfully");
     }
     /**
      * Show the form for editing the specified resource.
@@ -31,7 +31,7 @@ class AnswersController extends Controller
     public function edit(Question $question, Answer $answer)
     {
         $this->authorize('update', $answer);
-        return view('answers.edit',compact('question','answer'));
+        return view('answers.edit', compact('question', 'answer'));
     }
 
     /**
@@ -41,13 +41,20 @@ class AnswersController extends Controller
      * @param  \App\Answer  $answer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request,Question $question, Answer $answer)
+    public function update(Request $request, Question $question, Answer $answer)
     {
-            $this->authorize('update',$answer);
-            $answer->update($request->validate([
-                'body' => 'required'
-            ]));
-            return redirect()->route('questions.show', $question->slug)->with('success','You have updated your answer successfully');
+        $this->authorize('update', $answer);
+        $answer->update($request->validate([
+            'body' => 'required'
+        ]));
+        if ($request->expectsJson()) {
+            return response()->json([
+                'message' => 'You have updated your answer successfully',
+                'body_html' => $answer->body_html
+            ]);
+        }
+        // return redirect()->route('questions.show', $question->slug)->with('success','You have updated your answer successfully');
+
     }
 
     /**
@@ -58,8 +65,8 @@ class AnswersController extends Controller
      */
     public function destroy(Question $question, Answer $answer)
     {
-        $this->authorize('delete',$answer);
+        $this->authorize('delete', $answer);
         $answer->delete();
-        return back()->with('success','Answer have been deleted successfully');
+        return back()->with('success', 'Answer have been deleted successfully');
     }
 }
